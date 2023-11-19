2f5705dd61a127e25f224cbb06af6ce7f4926696
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-11-20 02:35:53 +0900

    회원가입시 중복유저 체크

--- user/views.py
+++ user/views.py
@@ -4,28 +4,41 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 from .forms import UserForm
 
-def user_view(request):
+def signup_view(request):
+    form = UserForm(request.POST or None)
     if request.method == "POST":
-        if 'signup' in request.POST:
-            signup_form = UserForm(request.POST)
-            if signup_form.is_valid():
-                user = User.objects.create_user(
-                    id=signup_form.cleaned_data['id'],
-                    password=signup_form.cleaned_data['password'],
-                    nickname=signup_form.cleaned_data['nickname'],
-                )
-                login(request, user)
-                return redirect('/')
-        elif 'login' in request.POST:
-            login_form = AuthenticationForm(request, data=request.POST)
-            if login_form.is_valid():
-                login(request, login_form.get_user())
-                return redirect('/')
-        elif 'logout' in request.POST:
-            logout(request)
+        if form.is_valid():
+            if User.objects.filter(id=form.cleaned_data['id']).exists():
+                form.add_error('id', '이미 존재하는 아이디입니다.')
+                return render(request, 'user/user.html', {'form': form})
+            if User.objects.filter(nickname=form.cleaned_data['nickname']).exists():
+                form.add_error('nickname', '이미 존재하는 닉네임입니다.')
+                return render(request, 'user/user.html', {'form': form})
+
+            user = User.objects.create_user(
+                id=form.cleaned_data['id'],
+                password=form.cleaned_data['password'],
+                nickname=form.cleaned_data['nickname'],
+            )
+            login(request, user)
             return redirect('/')
     else:
-        signup_form = UserForm()
-        login_form = AuthenticationForm()
+        form = UserForm()
 
-    return render(request, 'user/user.html', {'signup_form': signup_form, 'login_form': login_form})
+    return render(request, 'user/signup.html', {'form': form})
+
+def login_view(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'user/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('/')
